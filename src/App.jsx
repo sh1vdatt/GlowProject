deed9b796b6c69aa0ba02d83cf0680b0a482bdea
sh1vdatt <shivdattchaudhary1711@gmail.com>
2025-03-13 02:22:04 +0530

    Applied routing for ProductChoice file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./styles/index.css";
 import { Home } from "./components/sections/Home";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Authentication from "./components/signup/Authentication";
+import ProductSelection from "./components/signup/ProductChoice";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Authentication />} />
+        <Route path="/product-choice" element={<ProductSelection />} />
       </Routes>
     </BrowserRouter>
   );
